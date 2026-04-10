cc6a46827497b198672e8acad548e917a72db31a
Hiruna Dissanayake <118476665+hirunadissanayake@users.noreply.github.com>
2026-04-10 20:07:53 +0530

    chore: update services/iot-server-service/src/main/java/com/agms/iot/IotServerServiceApplication.java

---
+++ services/iot-server-service/src/main/java/com/agms/iot/IotServerServiceApplication.java
@@ -0,0 +1,11 @@
+package com.agms.iot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotServerServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IotServerServiceApplication.class, args);
+    }
+}
